f5da67daf92fb8b219b0b7eb3565290e3dc3a7b1
Meridius88 <kemono.meridius@gmail.com>
2024-12-21 19:30:46 +0400

    Changes to be committed:

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './table/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"tables/home", component:HomeComponent},
+    {path:"tables",redirectTo: "tables/home",pathMatch:"full"},
+    {path:"",redirectTo: "tables/home",pathMatch:"full"}
+];
---
+++ src/app/table/table.ts
@@ -0,0 +1,6 @@
+export interface Table {
+    number: Number,
+    capacity: Number,
+    inoutdoor:String,
+    available: Boolean
+}
